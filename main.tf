75d42f8e293448a9a9a2cb33f1bcb0009c5b6d88
Dmitriy Firsov <dmitriy.firsov@flatstack.com>
2022-04-22 16:23:13 +0300

    create s3 bucket and pass handy passing env variables

--- main.tf
+++ main.tf
@@ -19,16 +19,16 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
 }
 
-resource "random_pet" "lambda_bucket_name" {
-  prefix = "learn-terraform-functions"
+resource "random_pet" "lambda_bucket" {
+  prefix = "hello-world"
   length = 4
 }
 
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = random_pet.lambda_bucket_name.id
-
-  acl           = "private"
+  bucket = random_pet.lambda_bucket.id
   force_destroy = true
 }
--- variables.tf
+++ variables.tf
@@ -2,7 +2,16 @@
 
 variable "aws_region" {
   description = "AWS region for all resources."
-
   type    = string
   default = "us-east-1"
+}
+
+variable "AWS_ACCESS_KEY" {
+  description = "aws account id"
+  type = string
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "aws secret key"
+  type = string
 }
